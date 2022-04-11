0c40a072abe4538eabc7a300aa6345f8b65b0074
DEVJS2005 <79548150+DEVJS2005@users.noreply.github.com>
2022-04-11 15:43:04 -0300

    3º aulas do curso

---
+++ Mercadinho.Prateleira/src/Mercadinho.Infrastructure.Data/PrateleiraDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mercadinho.Prateleira.Domain;
+
+namespace Mercadinho.Infrastructure.Data
+{
+    public class PrateleiraDbContext : DbContext
+    {
+        public PrateleiraDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Estoque> Estoques { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(PrateleiraDbContext).Assembly);
+        }
+    }
+}
